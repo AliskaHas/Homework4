5396e75a0bf1cec55bb388e838b82a63604e6ad4
AliskaHas <nhasanova@mail.ru>
2023-06-27 00:50:35 +0300

    Возведение числа А в степень числа В

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int ExpNumbers(int A, int B)
+{
+    int exp = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        exp = exp * A;
+    }
+    return exp;
+}
+
+//--------------основная часть кода---------------
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+
+int exp = ExpNumbers(numberA,numberB);
+
+System.Console.WriteLine($"Число {numberA} в степени {numberB} равно {exp}");
