fd5777ec8d0d480b99b829c20184e842779ca7b4
AliskaHas <nhasanova@mail.ru>
2023-06-27 03:17:30 +0300

    Вывод чисел

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+        for(int index = 0; index < array.Length; index++)
+        {
+             array[index] = rand.Next(0,99);  
+        }
+}
+
+
+int[] array = new int[8];
+FillArray(array);
+System.Console.WriteLine(string.Join(", ", array));
